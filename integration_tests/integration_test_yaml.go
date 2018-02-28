4ebdfb63f256a63324b180d65bcbf4410d4bcfd5
Priya Wadhwa <priyawadhwa@google.com>
2018-02-28 12:01:52 -0800

    Refactored to use config json isntead of go file

--- integration_tests/integration_test_yaml.go
+++ integration_tests/integration_test_yaml.go
@@ -19,18 +19,19 @@ package main
 import (
 	"fmt"
 	"gopkg.in/yaml.v2"
-	"path/filepath"
 )
 
 var tests = []struct {
 	description    string
 	dockerfilePath string
+	configPath     string
 	context        string
 	repo           string
 }{
 	{
 		description:    "test extract filesystem",
-		dockerfilePath: "dockerfiles/Dockerfile",
+		dockerfilePath: "/workspace/integration_tests/dockerfiles/Dockerfile_test_extract_fs",
+		configPath:     "/workspace/integration_tests/dockerfiles/config_test_extract_fs.json",
 		context:        "integration_tests/dockerfiles/",
 		repo:           "extract-filesystem",
 	},
@@ -49,9 +50,12 @@ type testyaml struct {
 var executorImage = "executor-image"
 var executorCommand = "/workspace/executor"
 var dockerImage = "gcr.io/cloud-builders/docker"
+var ubuntuImage = "ubuntu"
 var testRepo = "gcr.io/kbuild-test/"
 var dockerPrefix = "docker-"
 var kbuildPrefix = "kbuild-"
+var daemonPrefix = "daemon://"
+var containerDiffOutputFile = "container-diff.json"
 
 func main() {
 
@@ -61,13 +65,13 @@ func main() {
 		Args: []string{"cp", "gs://container-diff/latest/container-diff-linux-amd64", "."},
 	}
 	containerDiffPermissions := step{
-		Name: "ubuntu",
+		Name: ubuntuImage,
 		Args: []string{"chmod", "+x", "container-diff-linux-amd64"},
 	}
 	// Build executor image
 	buildExecutorImage := step{
 		Name: dockerImage,
-		Args: []string{"build", "-t", executorImage, "."},
+		Args: []string{"build", "-t", executorImage, "-f", "integration_tests/executor/Dockerfile", "."},
 	}
 
 	y := testyaml{
@@ -75,32 +79,41 @@ func main() {
 	}
 	for _, test := range tests {
 		// First, build the image with docker
-		var dockerfilePath = filepath.Join("/workspace/integration_tests", test.dockerfilePath)
 		dockerBuild := step{
 			Name: dockerImage,
-			Args: []string{"build", "-t", testRepo + dockerPrefix + test.repo, "-f", dockerfilePath, test.context},
+			Args: []string{"build", "-t", testRepo + dockerPrefix + test.repo, "-f", test.dockerfilePath, test.context},
 		}
 
 		// Then, buld the image with kbuild and commit it
 		var commitID = "test"
 		kbuild := step{
 			Name: dockerImage,
-			Args: []string{"run", "-v", dockerfilePath + ":/workspace/Dockerfile", "--name", commitID, executorImage, executorCommand},
+			Args: []string{"run", "-v", test.dockerfilePath + ":/workspace/Dockerfile", "--name", commitID, executorImage, executorCommand},
 		}
 
 		commit := step{
 			Name: dockerImage,
 			Args: []string{"commit", commitID, testRepo + kbuildPrefix + test.repo},
 		}
-		y.Steps = append(y.Steps, dockerBuild, kbuild, commit)
-	}
 
-	integrationTests := step{
-		Name: "gcr.io/cloud-builders/go:debian",
-		Args: []string{"test", "integration_tests/integration_test.go"},
-		Env:  []string{"GOPATH=/", "PATH=/builder/bin:/go/bin:/usr/local/go/bin:/workspace"},
+		dockerImage := daemonPrefix + testRepo + dockerPrefix + test.repo
+		kbuildImage := daemonPrefix + testRepo + kbuildPrefix + test.repo
+		// Run container diff on the images
+		args := "container-diff-linux-amd64 diff " + dockerImage + " " + kbuildImage + " --type=file -j > " + containerDiffOutputFile
+		containerDiff := step{
+			Name: ubuntuImage,
+			Args: []string{"sh", "-c", args},
+			Env:  []string{"PATH=/workspace:/bin"},
+		}
+
+		// Compare output files
+		compareOutputs := step{
+			Name: ubuntuImage,
+			Args: []string{"cmp", test.configPath, containerDiffOutputFile},
+		}
+
+		y.Steps = append(y.Steps, dockerBuild, kbuild, commit, containerDiff, compareOutputs)
 	}
-	y.Steps = append(y.Steps, integrationTests)
 
 	d, _ := yaml.Marshal(&y)
 	fmt.Println(string(d))
