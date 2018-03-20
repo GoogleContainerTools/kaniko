7bc750d79348ca251724a70ed9ca3a90a56dee14
sharifelgamal <selgamal@google.com>
2018-03-20 14:27:58 -0700

    add check for valid protocol

--- pkg/commands/expose_test.go
+++ pkg/commands/expose_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package commands
 
 import (
@@ -27,6 +43,19 @@ func TestUpdateExposedPorts(t *testing.T) {
 		"8083/udp": {},
 	}
 
-	updateExposedPorts(ports, cfg)
-	testutil.CheckErrorAndDeepEqual(t, false, nil, expectedPorts, cfg.ExposedPorts)
+	err := updateExposedPorts(ports, cfg)
+	testutil.CheckErrorAndDeepEqual(t, false, err, expectedPorts, cfg.ExposedPorts)
+}
+
+func TestInvalidProtocol(t *testing.T) {
+	cfg := &manifest.Schema2Config{
+		ExposedPorts: manifest.Schema2PortSet{},
+	}
+
+	ports := []string{
+		"80/garbage",
+	}
+
+	err := updateExposedPorts(ports, cfg)
+	testutil.CheckErrorAndDeepEqual(t, true, err, nil, nil)
 }
