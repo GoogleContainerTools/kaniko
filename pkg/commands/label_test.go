f352583bc1533c8792d58840c5928509c4f984f1
sharifelgamal <selgamal@google.com>
2018-03-21 11:45:41 -0700

    boilerplate

--- pkg/commands/label_test.go
+++ pkg/commands/label_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package commands
 
 import (
@@ -9,23 +25,23 @@ import (
 
 func TestUpdateLabels(t *testing.T) {
 	cfg := &manifest.Schema2Config{
-		Labels: map[string]string {
+		Labels: map[string]string{
 			"foo": "bar",
 		},
 	}
 
 	labels := []instructions.KeyValuePair{
 		{
-			Key: "foo",
+			Key:   "foo",
 			Value: "override",
 		},
 		{
-			Key: "bar",
+			Key:   "bar",
 			Value: "baz",
 		},
 	}
 
-	expectedLabels := map[string]string {
+	expectedLabels := map[string]string{
 		"foo": "override",
 		"bar": "baz",
 	}
