27f964957baf5bf61b6384e6c8982afd5d24f027
Priya Wadhwa <priyawadhwa@google.com>
2018-04-04 17:06:01 -0700

    Support for onbuild and unit test

---
+++ pkg/commands/onbuild.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package commands
+
+import (
+	"github.com/GoogleCloudPlatform/k8s-container-builder/pkg/util"
+	"github.com/containers/image/manifest"
+	"github.com/docker/docker/builder/dockerfile/instructions"
+	"github.com/sirupsen/logrus"
+)
+
+type OnBuildCommand struct {
+	cmd *instructions.OnbuildCommand
+}
+
+func (o *OnBuildCommand) ExecuteCommand(config *manifest.Schema2Config) error {
+	logrus.Info("cmd: ONBUILD")
+	resolvedExpression, err := util.ResolveEnvironmentReplacement(o.cmd.Expression, config.Env, false)
+	if err != nil {
+		return err
+	}
+	if config.OnBuild == nil {
+		config.OnBuild = []string{resolvedExpression}
+	} else {
+		config.OnBuild = append(config.OnBuild, resolvedExpression)
+	}
+	return nil
+}
+
+// No files have changed, this command only touches metadata.
+func (o *OnBuildCommand) FilesToSnapshot() []string {
+	return []string{}
+}
+
+// CreatedBy returns some information about the command for the image config history
+func (o *OnBuildCommand) CreatedBy() string {
+	return o.cmd.Expression
+}
---
+++ pkg/commands/onbuild_test.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package commands
+
+import (
+	"github.com/GoogleCloudPlatform/k8s-container-builder/testutil"
+	"github.com/containers/image/manifest"
+	"github.com/docker/docker/builder/dockerfile/instructions"
+	"testing"
+)
+
+var onbuildTests = []struct {
+	expression    string
+	onbuildArray  []string
+	expectedArray []string
+}{
+	{
+		expression:   "RUN echo \\\"hi\\\" > $dir",
+		onbuildArray: nil,
+		expectedArray: []string{
+			"RUN echo \"hi\" > /some/dir",
+		},
+	},
+	{
+		expression: "COPY foo foo",
+		onbuildArray: []string{
+			"RUN echo \"hi\" > /some/dir",
+		},
+		expectedArray: []string{
+			"RUN echo \"hi\" > /some/dir",
+			"COPY foo foo",
+		},
+	},
+}
+
+func TestExecuteOnbuild(t *testing.T) {
+	for _, test := range onbuildTests {
+		cfg := &manifest.Schema2Config{
+			Env: []string{
+				"dir=/some/dir",
+			},
+			OnBuild: test.onbuildArray,
+		}
+
+		onbuildCmd := &OnBuildCommand{
+			&instructions.OnbuildCommand{
+				Expression: test.expression,
+			},
+		}
+
+		err := onbuildCmd.ExecuteCommand(cfg)
+		testutil.CheckErrorAndDeepEqual(t, false, err, test.expectedArray, cfg.OnBuild)
+	}
+
+}
