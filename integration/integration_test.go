f3ea031671e26fada660b6e858c64758fd2c5411
sharifelgamal <selgamal@google.com>
2018-05-24 11:07:26 -0700

    adding todo to remove uncomment failing test

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -107,6 +107,7 @@ func TestRun(t *testing.T) {
 
 	bucketContextTests := []string{"Dockerfile_test_copy_bucket"}
 
+	// TODO: remove test_user_run from this when https://github.com/GoogleContainerTools/container-diff/issues/237 is fixed
 	testsToIgnore := []string{"Dockerfile_test_user_run"}
 
 	_, ex, _, _ := runtime.Caller(0)
