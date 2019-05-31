c2304925821aee9f95a155034919be0aa9ff3040
SunLetian <sunletian@inspur.com>
2019-05-31 15:12:36 +0800

    Update push.go

--- pkg/executor/push.go
+++ pkg/executor/push.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2018 Google LLC
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,7 +56,7 @@ func CheckPushPermissions(opts *config.KanikoOptions) error {
 	for _, destination := range opts.Destinations {
 		destRef, err := name.NewTag(destination, name.WeakValidation)
 		if opts.Insecure {
-			destRef.Registry,err = name.NewInsecureRegistry(destRef.RegistryStr(),name.WeakValidation)
+			destRef.Registry, err = name.NewInsecureRegistry(destRef.RegistryStr(), name.WeakValidation)
 			if err != nil {
 				return errors.Wrap(err, "getting new insecure registry")
 			}
