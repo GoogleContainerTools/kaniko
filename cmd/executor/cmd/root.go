11703f51112accd796708716dab87deb2dac2b5b
Christopher Jackson <darkdragn.cj@gmail.com>
2019-06-29 21:13:12 +0000

    docs(registry-tls): update for the cli docstring for registry-tls cli switch

--- cmd/executor/cmd/root.go
+++ cmd/executor/cmd/root.go
@@ -134,7 +134,7 @@ func addKanikoOptionsFlags(cmd *cobra.Command) {
 	RootCmd.PersistentFlags().DurationVarP(&opts.CacheTTL, "cache-ttl", "", time.Hour*336, "Cache timeout in hours. Defaults to two weeks.")
 	RootCmd.PersistentFlags().VarP(&opts.InsecureRegistries, "insecure-registry", "", "Insecure registry using plain HTTP to push and pull. Set it repeatedly for multiple registries.")
 	RootCmd.PersistentFlags().VarP(&opts.SkipTLSVerifyRegistries, "skip-tls-verify-registry", "", "Insecure registry ignoring TLS verify to push and pull. Set it repeatedly for multiple registries.")
-	RootCmd.PersistentFlags().VarP(&opts.RegistryTLS, "registry-tls", "", "A TLS certificate for a registry.")
+	RootCmd.PersistentFlags().VarP(&opts.RegistryTLS, "registry-tls", "", "A TLS certificate for a registry. (registry=my.registry,cert=/mnt/cert.crt,key=/mnt/cert.key)")
 }
 
 // addHiddenFlags marks certain flags as hidden from the executor help text
