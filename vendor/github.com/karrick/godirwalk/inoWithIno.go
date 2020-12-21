f83de01da73f0d91de7d39d95c819605c44f1e2a
Tejal Desai <tejal29@gmail.com>
2020-12-21 15:30:47 -0800

    Update GowalkDir dependency to pick up fixes. Added default 90 min timeout for walking FS. Override it with environment variable. Add slowjam to kaniko pod

--- vendor/github.com/karrick/godirwalk/inoWithIno.go
+++ vendor/github.com/karrick/godirwalk/inoWithIno.go
@@ -1,9 +1,9 @@
-// +build darwin linux
+// +build aix darwin linux nacl solaris
 
 package godirwalk
 
 import "syscall"
 
 func inoFromDirent(de *syscall.Dirent) uint64 {
-	return de.Ino
+	return uint64(de.Ino)
 }
