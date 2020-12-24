9d7d0855d1b59f015d38404165a29d2593a5b35e
Tejal Desai <tejal29@gmail.com>
2020-12-24 10:26:01 -0800

    move CI job back to docker

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -22,6 +22,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+
+mkdir -p $HOME/.docker/
 echo '{"experimental": "enabled"}' > $HOME/.docker/config.json
 echo '{"experimental":"enabled"}' | sudo tee /etc/docker/daemon.json
 
