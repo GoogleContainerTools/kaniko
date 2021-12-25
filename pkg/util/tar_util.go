6e1b450f3091171912c8d9a21ff14130090c7812
Andrei Kvapil <kvapss@gmail.com>
2021-12-25 10:28:34 +0100

    export UnTar function

--- pkg/util/tar_util.go
+++ pkg/util/tar_util.go
@@ -218,7 +218,7 @@ func UnpackLocalTarArchive(path, dest string) ([]string, error) {
 			return nil, UnpackCompressedTar(path, dest)
 		} else if compressionLevel == archive.Bzip2 {
 			bzr := bzip2.NewReader(file)
-			return unTar(bzr, dest)
+			return UnTar(bzr, dest)
 		}
 	}
 	if fileIsUncompressedTar(path) {
@@ -227,7 +227,7 @@ func UnpackLocalTarArchive(path, dest string) ([]string, error) {
 			return nil, err
 		}
 		defer file.Close()
-		return unTar(file, dest)
+		return UnTar(file, dest)
 	}
 	return nil, errors.New("path does not lead to local tar archive")
 }
