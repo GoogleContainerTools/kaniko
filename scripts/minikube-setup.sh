d41f43845a1ee60edf54166313cffad9aa451875
Höhl, Lukas <lukas.hoehl@accso.de>
2022-06-28 13:16:20 +0200

    cleanup
    Signed-off-by: Höhl, Lukas <lukas.hoehl@accso.de>

--- scripts/minikube-setup.sh
+++ scripts/minikube-setup.sh
@@ -63,13 +63,6 @@ sudo apt-get update
 sudo apt-get install -y liblz4-tool
 cat /proc/cpuinfo
 
-# need to run minikube start as root since user doesn't have permissions to communicate with docker in minikube 1.26
-# See https://minikube.sigs.k8s.io/docs/drivers/none/#other 
-minikube start --v=8 --vm-driver=none --force --addons="registry,default-storageclass,storage-provisioner" || minikube logs;
-
-# move minikube stuff back to user directory
-# sudo mv /root/.kube /root/.minikube $HOME
-# sudo chown -R $USER $HOME/.kube $HOME/.minikube 
-
+minikube start --vm-driver=none --force --addons="registry,default-storageclass,storage-provisioner" || minikube logs;
 minikube status
 kubectl cluster-info
