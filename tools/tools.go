93b8adf78e3599a350a4c797c43b9f58457cbed2
Liam Newman <96086065+liam-verta@users.noreply.github.com>
2023-06-12 15:01:50 -0700

    Add comment

--- tools/tools.go
+++ tools/tools.go
@@ -2,7 +2,8 @@
 
 package tools
 
-//  dependencies https://github.com/golang/go/issues/48332
+// dependencies https://github.com/golang/go/issues/48332
+// These are placeholder imports the make go mod include these tools in its dependency graph.
 import (
 	_ "github.com/GoogleCloudPlatform/docker-credential-gcr"
 	_ "github.com/awslabs/amazon-ecr-credential-helper/ecr-login/cli/docker-credential-ecr-login"
