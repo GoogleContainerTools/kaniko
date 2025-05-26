ac6ccd1513dabf91e53b9187687801661bc67961
Martin Zihlmann <martizih@outlook.com>
2025-05-26 23:41:24 +0100

    use args overrides to minimize diff

--- integration/images.go
+++ integration/images.go
@@ -87,6 +87,10 @@ var additionalKanikoFlagsMap = map[string][]string{
 	"Dockerfile_test_maintainer":             {"--single-snapshot"},
 	"Dockerfile_test_target":                 {"--target=second"},
 	"Dockerfile_test_snapshotter_ignorelist": {"--use-new-run=true", "-v=trace"},
+	"Dockerfile_test_cache":                  {"--cache-copy-layers=true"},
+	"Dockerfile_test_cache_oci":              {"--cache-copy-layers=true"},
+	"Dockerfile_test_cache_install":          {"--cache-copy-layers=true"},
+	"Dockerfile_test_cache_install_oci":      {"--cache-copy-layers=true"},
 	"Dockerfile_test_cache_copy":             {"--cache-copy-layers=true"},
 	"Dockerfile_test_cache_copy_oci":         {"--cache-copy-layers=true"},
 }
